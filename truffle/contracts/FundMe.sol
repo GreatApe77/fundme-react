08fca3ab8e6eb458e2f8b94ed5cf34820165b922
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-03-17 23:53:55 -0300

    progresso no FundMe.sol

--- truffle/contracts/FundMe.sol
+++ truffle/contracts/FundMe.sol
@@ -2,21 +2,50 @@
 pragma solidity ^0.8.2;
 
 contract FundMe {
-
-    struct Funder{
+    struct Funder {
         address addressOfFunder;
         uint256 amountFunded;
     }
 
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
 
+    address private owner;
 
-    mapping(address=>Funder) private funderByAddress;
+    mapping(address => Funder) private funderByAddress;
 
+    constructor() {
+        owner = msg.sender;
+    }
 
-    function getContractBalance() public view returns(uint256){
+    function getContractBalance() public view returns (uint256) {
         return address(this).balance;
     }
 
+    function getFunder(
+        address funderAddress
+    ) public view returns (Funder memory) {
+        return funderByAddress[funderAddress];
+    }
 
+    function fund() public payable {
+        funderByAddress[msg.sender].addressOfFunder = msg.sender;
 
+        funderByAddress[msg.sender].amountFunded += msg.value;
+    }
+    //dono pode scar os ganhos do contrato
+    function withdraw(uint256 amount) external onlyOwner(){
+        
+    }
+
+    receive() external payable{
+        fund();
+    }
+
+    
+    fallback() external payable{
+        fund();
+    }
 }
