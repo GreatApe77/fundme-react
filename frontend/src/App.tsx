5f208ebe75be3b3d22e87d75c2166baa05ade37a
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-09-29 01:30:35 -0300

    formata o codigo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,16 @@
-import Header from "./components/Header"
+import Header from "./components/Header";
 
 import Hero from "./components/Hero";
 
-
-
 function App() {
-  
-  
-  
-  
-  return (
-    <div >
-    <Header/>
-    <div >
-    <Hero/>
-
-    </div>
-      
-     
-     
-     
-     
-    
-     
-    </div>
-  )
+	return (
+		<div>
+			<Header />
+			<div>
+				<Hero />
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
